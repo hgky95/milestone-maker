698ec505f067b42b66181064c85df23941a3bc46
Ky Huynh <hgky95@gmail.com>
2024-08-09 23:04:26 -0400

    refactor ipfs handler and hiveagent to support multi tasks

--- src/ai-agent/ipfs_handler.py
+++ src/ai-agent/ipfs_handler.py
@@ -1,29 +1,30 @@
 import os
 import requests
 from dotenv import load_dotenv
-import json
 
 # Load environment variables
 load_dotenv()
 
-def pin_json_to_ipfs(json_data):
-    url = "https://api.pinata.cloud/pinning/pinJsonToIPFS"
-    print(os.getenv('PINATA_API_KEY'))
+PINATA_HOST = "https://api.pinata.cloud/pinning/pinJsonToIPFS"
+
+
+def pin_json_to_ipfs(json_obj):
+    url = PINATA_HOST
     headers = {
         'pinata_api_key': os.getenv('PINATA_API_KEY'),
         'pinata_secret_api_key': os.getenv('PINATA_SECRET_API_KEY')
     }
 
-    response = requests.post(url, headers=headers, json=json_data)
+    print("Upload json to IPFS: ", json_obj)
+    response = requests.post(url, headers=headers, json=json_obj)
     res_json = response.json()
     print('res_json: ', res_json)
 
-    token_uri = f"https://gateway.pinata.cloud/ipfs/{res_json['IpfsHash']}"
-    print("Token URI", token_uri)
-    return token_uri
-
+    cid = res_json['IpfsHash']
+    print("IPFS hash", cid)
+    return cid
 
 # Example usage
-if __name__ == "__main__":
-    data = {"key": "value"}
-    pin_json_to_ipfs(data)
+# if __name__ == "__main__":
+#     data = {"key": "value"}
+#     pin_json_to_ipfs(data)
