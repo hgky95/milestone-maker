ea8fd3bcff6f75e0800dfb0ccd293dc0ddcca3af
Ky Huynh <hgky95@gmail.com>
2024-08-19 23:19:15 -0400

    update smart contract to support nft enumerable

--- src/smartcontract/src/MileStoneMaker.sol
+++ src/smartcontract/src/MileStoneMaker.sol
@@ -3,9 +3,10 @@ pragma solidity ^0.8.25;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MileStoneMaker is ERC721URIStorage, Ownable {
+contract MileStoneMaker is ERC721, ERC721URIStorage, ERC721Enumerable, Ownable {
     uint256 private _tokenIds;
     uint256 private _learningPathIds;
 
@@ -174,5 +175,37 @@ contract MileStoneMaker is ERC721URIStorage, Ownable {
     ) public view returns (bool[] memory) {
         LearningPath storage path = userLearningPaths[_user][_learningPathId];
         return path.milestones;
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function _increaseBalance(
+        address account,
+        uint128 value
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._increaseBalance(account, value);
+    }
+
+    function _update(
+        address to,
+        uint256 tokenId,
+        address auth
+    ) internal override(ERC721, ERC721Enumerable) returns (address) {
+        return super._update(to, tokenId, auth);
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    )
+        public
+        view
+        override(ERC721, ERC721URIStorage, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
